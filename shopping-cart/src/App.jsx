089ca35eaf8c279b96e8c119ebcc88219c32c5f0
robsomers900 <64647442+robsomers900@users.noreply.github.com>
2025-02-27 23:18:58 +0000

    Add external data fetching

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,28 +1,89 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Home from './home'
 import {Link, Outlet} from "react-router-dom"
 
+const useProduct = () => {
+  const [productData, setProductData] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products', {mode: 'cors'})
+    .then((response) => {
+      if (response.status > 400) {
+        throw new Error("server error")
+      }
+      return response.json()
+    })
+    .then((response) => setProductData([{
+      item: response[0].name,
+      name: response[0].name, //update this to fetch from api
+      description: response[0].description, //update this to fetch from api
+      price: response[0].price, //update this to fetch from api
+      quantity: 0
+    },
+    {
+      item: response[1].name,
+      name: response[1].name, //update this to fetch from api
+      description: response[1].description, //update this to fetch from api
+      price: response[1].price, //update this to fetch from api
+      quantity: 0
+    }
+  ])
+    )
+    .catch((error) => setError(error))
+    .finally(() => setLoading(false))
+  },
+  []
+  )
+  
+  return{productData, error, loading}
+}
+
+
+
 function App() {
   const [count, setCount] = useState(0)
   const [order, setOrder] = useState([])
-  const [cards, setCards] = useState([
-    {
-        item: "kettle",
-        name: "Kettle",
-        description: "Boils Water",
-        price: 10,
-        quantity: 0
-    },
-    {
-        item: "toaster",
-        name: "Toaster",
-        description: "Cooks Bread",
-        price: 15,
-        quantity: 0
-    }
-])
+  const{productData, loading, error} = useProduct()
+  const[cards, setCards] = useState([])
+  // const [cards, setCards] = useState([
+  //   {
+  //       item: "kettle",
+  //       name: "Kettle", //update this to fetch from api
+  //       description: "Boils Water", //update this to fetch from api
+  //       price: 10, //update this to fetch from api
+  //       quantity: 0
+  //   },
+  //   {
+  //       item: "toaster",
+  //       name: "Toaster",
+  //       description: "Cooks Bread",
+  //       price: 15,
+  //       quantity: 0
+  //   }
+  // ])
+  //   useEffect(() => {
+  //     if(productData){
+  //       setCards({
+  //         productData
+  //       })
+  //     }
+  //   }, [productData]
+  // )
 
+  useEffect(() => {
+    if(productData.length > 0){
+      setCards(productData)
+    }
+  }, [productData])
+  
+  if(loading){
+    return(
+      <h1>Loading...</h1>
+    )
+  }
   return (
     <>
       <div className='topbar'>
@@ -48,7 +109,7 @@ function App() {
         </nav>
       </div>
       <div className='main-body'>
-        <Outlet context={{count, setCount, cards, setCards}}/>
+        <Outlet context={{count, setCount, cards, setCards, productData}}/>
       </div>
     </>
   )
